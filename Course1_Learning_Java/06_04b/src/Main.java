e633a6eb61c5f17ee7f53d8b35d53ee5e98fc982
RedietMillion <redietmillion14@gmail.com>
2022-08-03 14:00:27 +0300

    Java class

--- Course1_Learning_Java/06_04b/src/Main.java
+++ Course1_Learning_Java/06_04b/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+	     Triangle triA = new Triangle(15, 8, 15, 8, 9);
+	     Triangle triB = new Triangle(3, 2.598,3, 8, 9);
 
     }
 
--- Course1_Learning_Java/06_05b/src/Main.java
+++ Course1_Learning_Java/06_05b/src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
         Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
         Triangle triangleB = new Triangle(3, 2.598, 3, 3,3);
+	
+	double triangleArea = triangleA.findArea();
+	System.out.println(triangleArea);
+	double triangleBArea = triangleB.findArea();
+        System.out.println(triangleBArea);
 
     }
 
